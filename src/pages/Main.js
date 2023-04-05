d07763edff2f6733ee9ac41947453da1382e4cc2
Leo Mirete <l.mirete@net.estia.fr>
2023-04-05 11:54:42 +0200

    commit 1

---
+++ src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "../Components/Navigation";
+import Cards from "../Components/Cards";
+
+const Main = () => {
+    return (
+        <main>
+            <Navigation />
+            <div className="title">
+                <h1>NBA players</h1>
+            </div>
+        </main>
+    );
+};
+
+export default Main;
